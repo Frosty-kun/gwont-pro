328c0ebd0ffc7d97487d963bb8ad0b7089c07d77
Marcos Cepero <frostymcp@gmail.com>
2024-03-16 03:02:31 -0400

    Commit 1. Card sprites added

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Art;
+    public CardData cardData;
+    public GameObject Card1;
+    public GameObject Hand;
+
+    public void OnClick()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject card = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(Hand.transform, false);
+            card.name = $"{card.name}{i}";
+        }
+    }
+
+     public void SetArtImage(Sprite image)
+    {
+        if (Art == null || Art.GetComponent<Image>() == null)
+        {
+            Debug.LogError("Art GameObject or its Image component is missing.");
+            return;
+        }
+
+        Art.GetComponent<Image>().sprite = image;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
